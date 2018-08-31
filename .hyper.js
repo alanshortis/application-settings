40078ea74c964fc83956255529100ebc95cf8292
Alan Shortis <alan.shortis@mac.com>
2018-08-31 11:44:43 +0100

    Update font for hyper

--- .hyper.js
+++ .hyper.js
@@ -1,51 +1,51 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 13,
+    fontSize: 14,
 
     // font family with optional fallbacks
-    fontFamily: 'Fira Mono',
+    fontFamily: "OperatorMono-Light",
 
     // terminal cursor background color (hex)
-    cursorColor: '#F81CE5',
+    cursorColor: "#F81CE5",
 
     // color of the text
-    foregroundColor: '#fff',
+    foregroundColor: "#fff",
 
     // terminal background color
-    backgroundColor: '#000',
+    backgroundColor: "#000",
 
     // border color (window, tabs)
-    borderColor: '#333',
+    borderColor: "#333",
 
     // custom css to embed in the main window
-    css: '',
+    css: "",
 
     // custom css to embed in the terminal window
-    termCSS: '',
+    termCSS: "",
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: "12px 14px",
 
     // some color overrides. see http://bit.ly/29k1iU2 for
     // the full list
     colors: [
-      '#000000',
-      '#ff0000',
-      '#33ff00',
-      '#ffff00',
-      '#0066ff',
-      '#cc00ff',
-      '#00ffff',
-      '#d0d0d0',
-      '#808080',
-      '#ff0000',
-      '#33ff00',
-      '#ffff00',
-      '#0066ff',
-      '#cc00ff',
-      '#00ffff',
-      '#ffffff'
+      "#000000",
+      "#ff0000",
+      "#33ff00",
+      "#ffff00",
+      "#0066ff",
+      "#cc00ff",
+      "#00ffff",
+      "#d0d0d0",
+      "#808080",
+      "#ff0000",
+      "#33ff00",
+      "#ffff00",
+      "#0066ff",
+      "#cc00ff",
+      "#00ffff",
+      "#ffffff"
     ]
   },
 
@@ -55,11 +55,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [
-    'hyperterm-atom-dark',
-    'hyperterm-1password',
-    'hyper-statusline'
-  ],
+  plugins: ["hyperterm-atom-dark", "hyperterm-1password", "hyper-statusline"],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
